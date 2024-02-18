61137b483f0e8b5dce3e3108659e4024e9029594
homeg <homegor14@gmail.com>
2024-02-19 02:53:09 +0700

    button icon init

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -1,17 +1,22 @@
 import type { ReactNode } from 'react'
 
 import { classNames } from '@/shared'
+import { Icon } from '@/shared/ui/icon'
+// eslint-disable-next-line import/no-internal-modules
+import { IconName } from '@/shared/ui/icon/utils.ts'
 
 import cl from './Button.module.scss'
 
 export const Button = ({
 	onClick,
 	children,
-	className
+	className,
+	icon
 }: {
 	onClick: () => void
 	children: ReactNode
 	className: string
+	icon: IconName
 }) => {
 	return (
 		<>
@@ -19,6 +24,7 @@ export const Button = ({
 				onClick={onClick}
 				className={classNames(cl.button, className)}>
 				{children}
+				<Icon name={icon} />
 			</button>
 		</>
 	)
