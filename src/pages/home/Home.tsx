afc80cf44377135a71ef09f61c5319a56a3b21b4
andreyfeel83 <andreyfeel83@gmail.com>
2024-02-29 15:29:03 +0200

    add switch-component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,26 @@
-import { Switch } from "@/shared/ui/switch"
+import { Switch } from '@/shared/ui/switch'
 
 export const HomePage = () => {
-	return <Switch textOnLeft="Courses" textOnRight="My schedule"/>
+	return (
+		<>
+			<h1 style={{ textAlign: 'center', marginBottom: '30px' }}>
+				largeSize
+			</h1>
+
+			<Switch
+				textOnLeft="Courses"
+				textOnRight="My schedule"
+				variant="largeSize"
+			/>
+
+			<h1 style={{ textAlign: 'center', marginBottom: '30px' }}>
+				smallSize
+			</h1>
+			<Switch
+				textOnLeft="Courses"
+				textOnRight="My schedule"
+				variant="smallSize"
+			/>
+		</>
+	)
 }
--- src/shared/ui/switch/Switch.tsx
+++ src/shared/ui/switch/Switch.tsx
@@ -9,46 +9,93 @@ const { Text } = Typography
 export const Switch = ({
 	textOnLeft,
 	textOnRight,
+	variant,
 	checked
 }: {
 	textOnLeft: string
 	textOnRight: string
-	checked: boolean
+	variant: string
+	checked?: boolean
 }) => {
 	const [isActive, setIsActive] = useState<boolean>(checked ?? false)
 
 	const isSwitching = () => setIsActive(!isActive)
 
-	return (
-		<div className={cl.root}>
-			<div
-				className={classNames(
-					cl.root__body,
-					isActive ? cl.root__active : ''
-				)}
-				onClick={isSwitching}>
-				<Text
-					text={textOnLeft}
-					className={classNames(
-						cl.root__body_text,
-						isActive ? cl.root__active_text : ''
-					)}
-				/>
-			</div>
-			<div
-				className={classNames(
-					cl.root__body,
-					!isActive ? cl.root__active : ''
-				)}
-				onClick={isSwitching}>
-				<Text
-					text={textOnRight}
-					className={classNames(
-						cl.root__body_text,
-						!isActive ? cl.root__active_text : ''
-					)}
-				/>
-			</div>
-		</div>
-	)
+	variant === 'smallSize'
+	variant === 'largeSize'
+
+	switch (variant) {
+		case 'smallSize':
+			return (
+				<div className={cl.root}>
+					<div
+						className={classNames(
+							cl.root__body,
+							isActive ? cl.root__active : ''
+						)}
+						onClick={isSwitching}>
+						<Text
+							text={textOnLeft}
+							className={classNames(
+								cl.root__body_text,
+								isActive ? cl.root__active_text : ''
+							)}
+						/>
+					</div>
+					<div
+						className={classNames(
+							cl.root__body,
+							!isActive ? cl.root__active : ''
+						)}
+						onClick={isSwitching}>
+						<Text
+							text={textOnRight}
+							className={classNames(
+								cl.root__body_text,
+								!isActive ? cl.root__active_text : ''
+							)}
+						/>
+					</div>
+				</div>
+			)
+			break
+		case 'largeSize':
+			return (
+				<div className={classNames(cl.root, cl.root_large)}>
+					<div
+						className={classNames(
+							cl.root__body,
+							cl.root__body_large,
+							isActive ? cl.root__active : ''
+						)}
+						onClick={isSwitching}>
+						<Text
+							text={textOnLeft}
+							className={classNames(
+								cl.root__body_text,
+								isActive ? cl.root__active_text : ''
+							)}
+						/>
+					</div>
+					<div
+						className={classNames(
+							cl.root__body,
+							cl.root__body_large,
+							!isActive ? cl.root__active : ''
+						)}
+						onClick={isSwitching}>
+						<Text
+							text={textOnRight}
+							className={classNames(
+								cl.root__body_text,
+								!isActive ? cl.root__active_text : ''
+							)}
+						/>
+					</div>
+				</div>
+			)
+			break
+		default:
+			return ''
+	}
 }
