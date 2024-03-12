b7165feb8db9df5d81f5cef98ef71a0280e5962b
Eduard <>
2024-03-12 05:03:21 +0100

    complemented button, add Text, IconName, Variant, etc props

--- src/shared/ui/button/Button.tsx
+++ src/shared/ui/button/Button.tsx
@@ -1,20 +1,54 @@
-import { classNames } from '@/shared'
+import { Icon, IconName, classNames } from '@/shared'
+
+import { Typography } from '../typography'
 import cl from './Button.module.scss'
 
 export const Button = ({
-    children,
-	onClick,
-    className
+	className,
+	text,
+	iconName,
+	fullWidth = false,
+	thin = false,
+	variant = 'primary',
+	onClick
 }: {
+	className?: string
+	text?: string
+	iconName?: IconName
+	fullWidth?: boolean
+	thin?: boolean
+	variant?: 'primary' | 'secondary' | 'icon-primary' | 'icon-secondary'
 	onClick?: () => void
-    children: React.ReactNode
-    className?: string
 }) => {
+	const { Text } = Typography
+	const classNameVariant = {
+		primary: cl.root__primary,
+		secondary: cl.root__secondary,
+		'icon-primary': cl.root__icon_primary,
+		'icon-secondary': cl.root__icon_secondary
+	}
+	const classes = classNames(
+		cl.root,
+		className ?? '',
+		text ? cl.root__text : '',
+		text && iconName ? cl.root__text_icon : '',
+		iconName && !text ? cl.root__icon : '',
+		fullWidth ? cl.root_full : '',
+		thin ? cl.root_thin : '',
+		classNameVariant[variant]
+	)
+
 	return (
 		<button
-			onClick={onClick}
-			className={classNames(cl.root, className ?? '')}>
-			{children}
+			className={classes}
+			onClick={onClick}>
+			{text && (
+				<Text
+					variant="button"
+					text={text}
+				/>
+			)}
+			{iconName && <Icon name={iconName} />}
 		</button>
 	)
 }
--- src/shared/ui/typography/text/Text.tsx
+++ src/shared/ui/typography/text/Text.tsx
@@ -7,16 +7,23 @@ import cl from './Text.module.scss'
 export const Text = ({
 	className,
 	text,
-	noTranslate
+	noTranslate,
+	variant = 'default'
 }: {
 	className?: string
 	text: string
 	noTranslate?: boolean
+	variant?: 'default' | 'button'
 }) => {
 	const { t } = useTranslation()
 
 	return (
-		<p className={classNames(cl.root, className ?? '')}>
+		<p
+			className={classNames(
+				cl.root,
+				className ?? '',
+				variant === 'button' ? cl.root__button : ''
+			)}>
 			{noTranslate ? text : t(text)}
 		</p>
 	)
